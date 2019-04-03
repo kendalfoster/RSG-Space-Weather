d4c4c42ebc4e94e51cbffcec195e9ce25f8b853b
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2019-04-03 12:55:57 +0100

    index for times cleaning

--- First Pass/test2.py
+++ First Pass/test2.py
@@ -8,26 +8,42 @@ import xarray as xr
 data = pd.read_csv("20190403-00-22-supermag.csv")
 data = data.loc[0:35]
 readings = ['N', 'E', 'Z']
+
+
+
+## Function to restructure the data as a Dataset (xarray)
 stations = data['IAGA'].unique()
 num_st = stations.size
 times = data['Date_UTC'].unique()
 
-da = xr.DataArray(data = data[readings].loc[data['IAGA'] == stations[0]],
+temp_data = data[readings].loc[data['IAGA'] == stations[1]]
+idx = temp_data.index
+idx[1]
+
+da = xr.DataArray(data = temp_data,
                       coords = [times, readings],
                       dims = ['time', 'reading']
                       )
 
 for i in stations[1:]:
         print(i)
-        
-        temp_da = xr.DataArray(data = data[readings].loc[data['IAGA'] == i],
-                              coords = [times, readings],
+        temp_data = data[readings].loc[data['IAGA'] == i]
+        temp_times = clean_times(temp_data)
+        temp_da = xr.DataArray(data = temp_data,
+                              coords = [temp_times, readings],
                               dims = ['time', 'reading']
                               )
         da = xr.concat([da, temp_da], dim = 'station')
         da = da.transpose('time', 'reading', 'station')
 
 da
+
+
+## Function to clean up the times missed
+def clean_times(df):
+
+
+
 
 stations
 
