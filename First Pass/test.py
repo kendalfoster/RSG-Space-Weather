9f1cd98d3897fb49858893cd75156045bd6bc87c
kendalfoster <kendal.foster@warwick.ac.uk>
2019-04-04 04:52:00 +0100

    Added plotting function

--- First Pass/test.py
+++ First Pass/test.py
@@ -2,12 +2,14 @@
 import numpy as np
 import pandas as pd
 import xarray as xr # if gives error, just rerun
+import matplotlib.pyplot as plt
 
 
 ## Function to restructure the SuperMAG data as a Dataset (xarray)
 #       inputs: data- SuperMAG data as a pandas dataframe imported using read_csv
 #               readings- vector of characters representing measurements, default is ['N', 'E', 'Z']
 #               MLAT- input False if the magnetic latitude column is NOT included, default is inclusion of the column
+#       output: Dataset with the SuperMAG data easily accessible
 
 def mag_data_to_Dataset(data, readings=None, MLAT=None):
         if readings is None:
@@ -24,12 +26,12 @@ def mag_data_to_Dataset(data, readings=None, MLAT=None):
                 stations = sorted(stations, key=lambda x: x[1], reverse=True)
                 stations = [i[0] for i in stations]
 
-                times = data['Date_UTC'].unique()
+                # initialize DataArray (so we can append things to it later)
+                times = pd.to_datetime(data['Date_UTC'].unique())
                 cols = np.append('Date_UTC', readings)
 
-                # initialize DataArray (so we can append things to it later)
                 temp_data = data[cols].loc[data['IAGA'] == stations[0]]
-                temp_times = temp_data['Date_UTC'].unique()
+                temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
                 da = xr.DataArray(data = temp_data[readings],
                                       coords = [temp_times, readings],
                                       dims = ['time', 'reading']
@@ -38,7 +40,7 @@ def mag_data_to_Dataset(data, readings=None, MLAT=None):
                 # loop through the stations and append each to master DataArray
                 for i in stations[1:]:
                         temp_data = data[cols].loc[data['IAGA'] == i]
-                        temp_times = temp_data['Date_UTC'].unique()
+                        temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
                         temp_da = xr.DataArray(data = temp_data[readings],
                                                coords = [temp_times, readings],
                                                dims = ['time', 'reading']
@@ -56,12 +58,13 @@ def mag_data_to_Dataset(data, readings=None, MLAT=None):
         if MLAT is False:
                 stations = data['IAGA'].unique()
                 num_st = stations.size
-                times = data['Date_UTC'].unique()
-                cols = np.append('Date_UTC', readings)
 
                 # initialize DataArray (so we can append things to it later)
+                times = pd.to_datetime(data['Date_UTC'].unique())
+                cols = np.append('Date_UTC', readings)
+
                 temp_data = data[cols].loc[data['IAGA'] == stations[0]]
-                temp_times = temp_data['Date_UTC'].unique()
+                temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
                 da = xr.DataArray(data = temp_data[readings],
                                       coords = [temp_times, readings],
                                       dims = ['time', 'reading']
@@ -70,7 +73,7 @@ def mag_data_to_Dataset(data, readings=None, MLAT=None):
                 # loop through the stations and append each to master DataArray
                 for i in stations[1:]:
                         temp_data = data[cols].loc[data['IAGA'] == i]
-                        temp_times = temp_data['Date_UTC'].unique()
+                        temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
                         temp_da = xr.DataArray(data = temp_data[readings],
                                                coords = [temp_times, readings],
                                                dims = ['time', 'reading']
@@ -87,20 +90,32 @@ def mag_data_to_Dataset(data, readings=None, MLAT=None):
 
 
 
+## Function to plot the readings like on the SuperMAG website
+#       input: dataset output from mag_data_to_Dataset function
+#       output: series of plots, one per station, of the readings
+
+def plot_mag_data(ds):
+        ds.readings.plot.line(x='time', col='station', col_wrap=1)
+
+
+
 
 ## Testing
 data = pd.read_csv("20190403-00-22-supermag.csv")
 readings = ['N', 'E', 'Z']
 
 
-test_ds1 = mag_data_to_Dataset(data=data, readings=readings)
-test_ds1
+ds1 = mag_data_to_Dataset(data=data, readings=readings)
+ds1
 
-test_ds2 = mag_data_to_Dataset(data=data)
-test_ds2
+ds2 = mag_data_to_Dataset(data=data)
+ds2
 
-test_ds3 = mag_data_to_Dataset(data=data, MLAT=False)
-test_ds3
+ds3 = mag_data_to_Dataset(data=data, MLAT=False)
+ds3 # should be different order of stations to all the others
 
-test_ds4 = mag_data_to_Dataset(data=data, MLAT=True)
-test_ds4
+ds4 = mag_data_to_Dataset(data=data, MLAT=True)
+ds4
+
+
+plot_mag_data(ds=ds2)
