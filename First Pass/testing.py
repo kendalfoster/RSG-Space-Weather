7a1af66f02efbba80b6f0fffb470180d1945f565
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2019-04-13 16:20:12 +0100

    added intra-station cca

--- First Pass/testing.py
+++ First Pass/testing.py
@@ -48,129 +48,16 @@ import matplotlib.pyplot as plt
 import lib.supermag as sm
 import lib.rcca as rcca
 
-
-## Import Data
+## Import and Restructure SuperMAG Data
 ds = sm.mag_csv_to_Dataset(csv_file = "First Pass/20190403-00-22-supermag.csv",
                             MLT = True, MLAT = True)
 
-blc_n = ds.measurements.loc[dict(station = 'BLC')].loc[dict(reading = 'N')]
-blc_e = ds.measurements.loc[dict(station = 'BLC')].loc[dict(reading = 'E')]
-blc_z = ds.measurements.loc[dict(station = 'BLC')].loc[dict(reading = 'Z')]
+## CCA between stations
+test_inter = sm.inter_st_cca(ds = ds)
+test_inter
 
-blc_n1 = np.reshape(blc_n.values, newshape=[len(blc_n.values),1])
-blc_e1 = np.reshape(blc_e.values, newshape=[len(blc_e.values),1])
-blc_z1 = np.reshape(blc_z.values, newshape=[len(blc_z.values),1])
+## CCA between readings in one station
+test_intra = sm.intra_st_cca(ds = ds, station = 'BSL')
+test_intra
 
-blc = ds.measurements.loc[dict(station = 'BLC')]
-tal = ds.measurements.loc[dict(station = 'TAL')]
-ran = ds.measurements.loc[dict(station = 'RAN')]
-bsl = ds.measurements.loc[dict(station = 'BSL')]
-
-cca_ds = rcca.CCA(kernelcca = False, reg = 0., numCC = 1)
-cca_ds.train([blc, tal]).cancorrs[0]
-
-
-xr.concat([blc, bsl], dim = 'cca_st')
-
-aaa = xr.concat([blc,tal], dim = 'cca_st')
-ccc = rcca.CCA(kernelcca = False, reg = 0., numCC = 1).train(aaa).cancorrs[0]
-
-
-bbb = ds.measurements.loc[dict(station = stations[7])].values
-rm_nan = np.isfinite(bbb)
-rm_nan
-rm_nan[:,0]
-bbb[np.isfinite(bbb)].shape
-bbb[~rm_nan]
-
-x = [0,1,2,3]
-1 in x
-True in rm_nan
-
-# now turn the above into a function that operates on the whole dataset, ds
-def inter_st_cca(ds, readings=None):
-        # check if readings are provided
-        if readings is None:
-                readings = ['N', 'E', 'Z']
-
-        # universally necessary things
-        stations = ds.station
-        num_st = len(stations)
-        num_read = len(readings)
-
-        # setup (triangular) array for the correlation coefficients
-        cca_coeffs = np.zeros(shape = (num_st, num_st), dtype = float)
-
-        # shrinking nested for loops to get all the pairs of stations
-        for i in range(0, num_st-1):
-            first_st = ds.measurements.loc[dict(station = stations[i])]
-            for j in range(i+1, num_st):
-                second_st = ds.measurements.loc[dict(station = stations[j])]
-                # if False in np.isfinite(first_st) or False in np.isfinite(second_st):
-                comb_st = xr.concat([first_st, second_st], dim='reading')
-                # test stations for NaNs in the data (will mess up cca)
-                comb_st = comb_st.dropna(dim='time', how='any')
-                first_st = comb_st[:, 0:num_read]
-                second_st = comb_st[:, num_read:2*num_read]
-                # run cca
-                temp_cca = rcca.CCA(kernelcca = False, reg = 0., numCC = 1)
-                cca_coeffs[i,j] = temp_cca.train([first_st, second_st]).cancorrs[0]
-
-        # build DataArray from the cca_coeffs array
-        da = xr.DataArray(data = cca_coeffs,
-                          coords = [stations, stations],
-                          dims = ['first_st', 'second_st'])
-
-        # convert the DataArray into a Dataset
-        ds = da.to_dataset(name='cca_coeffs')
-
-        return ds
-
-ds = sm.mag_csv_to_Dataset(csv_file = "First Pass/20190403-00-22-supermag.csv",
-                            MLT = True, MLAT = True)
-test_coefs = sm.inter_st_cca(ds=ds)
-test_coefs
-
-
-
-
-##### from GitHub example
-# Initialize number of samples
-nSamples = 1000
-
-# Define two latent variables (number of samples x 1)
-latvar1 = np.random.randn(nSamples,)
-latvar2 = np.random.randn(nSamples,)
-
-# Define independent components for each dataset (number of observations x dataset dimensions)
-indep1 = np.random.randn(nSamples, 4)
-indep2 = np.random.randn(nSamples, 5)
-
-# Create two datasets, with each dimension composed as a sum of 75% one of the latent variables and 25% independent component
-data1 = 0.25*indep1 + 0.75*np.vstack((latvar1, latvar2, latvar1, latvar2)).T
-data2 = 0.25*indep2 + 0.75*np.vstack((latvar1, latvar2, latvar1, latvar2, latvar1)).T
-
-# Split each dataset into two halves: training set and test set
-train1 = data1[:nSamples//2]
-train2 = data2[:nSamples//2]
-test1 = data1[nSamples//2:]
-test2 = data2[nSamples//2:]
-
-train1.shape
-train2.shape
-
-# Create a cca object as an instantiation of the CCA object class.
-nComponents = 4
-cca = rcca.CCA(kernelcca = False, reg = 0., numCC = nComponents)
-
-# Use the train() method to find a CCA mapping between the two training sets.
-cca.train([train1, train2, train1])
-cca.cancorrs.shape
-cca.cancorrs
-
-# Use the validate() method to test how well the CCA mapping generalizes to the test data.
-# For each dimension in the test data, correlations between predicted and actual data are computed.
-testcorrs = cca.validate([test1, test2])
-testcorrs
-#####################
 ###############################################################################
--- lib/supermag.py
+++ lib/supermag.py
@@ -18,109 +18,108 @@ import lib.rcca as rcca
 #                       time is first dimension (ie, axis=0 for numpy commands)
 #                       data is accessible in array format via output.measurements
 #
-
 def mag_csv_to_Dataset(csv_file, readings=None, MLT=None, MLAT=None):
-        # get universally needed things
-        data = pd.read_csv(csv_file)
-        times = pd.to_datetime(data['Date_UTC'].unique())
+    # get universally needed things
+    data = pd.read_csv(csv_file)
+    times = pd.to_datetime(data['Date_UTC'].unique())
 
-        #-----------------------------------------------------------------------
-        #---------- optional arguments -----------------------------------------
-        #-----------------------------------------------------------------------
+    #-----------------------------------------------------------------------
+    #---------- optional arguments -----------------------------------------
+    #-----------------------------------------------------------------------
 
-        # check if readings are provided
-        if readings is None:
-                readings = ['N', 'E', 'Z']
+    # check if readings are provided
+    if readings is None:
+        readings = ['N', 'E', 'Z']
 
-        # if MLAT is included, sort and make Dataset
-        if MLAT is True:
-                # sort stations by magnetic latitude (from north to south)
-                stations = data['IAGA'].unique()
-                num_st = len(stations)
-                mlat_arr = np.vstack((stations,np.zeros(num_st))).transpose()
-                for i in range(0,num_st):
-                        mlat_arr[i,1] = data['MLAT'].loc[data['IAGA'] == stations[i]].mean()
-                mlat_arr = sorted(mlat_arr, key=lambda x: x[1], reverse=True)
-                stations = [i[0] for i in mlat_arr]
-                mlats = [round(i[1],4) for i in mlat_arr]
-                # build MLAT Dataset, for merging later
-                ds_mlat = xr.Dataset(data_vars = {'mlats': (['station'], mlats)},
-                                     coords = {'time': times,
-                                               'reading': readings,
-                                               'station': stations})
-        elif MLAT is not True:
-                stations = data['IAGA'].unique()
+    # if MLAT is included, sort and make Dataset
+    if MLAT is True:
+        # sort stations by magnetic latitude (from north to south)
+        stations = data['IAGA'].unique()
+        num_st = len(stations)
+        mlat_arr = np.vstack((stations,np.zeros(num_st))).transpose()
+        for i in range(0,num_st):
+            mlat_arr[i,1] = data['MLAT'].loc[data['IAGA'] == stations[i]].mean()
+        mlat_arr = sorted(mlat_arr, key=lambda x: x[1], reverse=True)
+        stations = [i[0] for i in mlat_arr]
+        mlats = [round(i[1],4) for i in mlat_arr]
+        # build MLAT Dataset, for merging later
+        ds_mlat = xr.Dataset(data_vars = {'mlats': (['station'], mlats)},
+                             coords = {'time': times,
+                                       'reading': readings,
+                                       'station': stations})
+    elif MLAT is not True:
+        stations = data['IAGA'].unique()
 
-        # if MLT (Magnetic Local Time) is included, make a Dataset
-        if MLT is True:
-                # initialize DataArray (so we can append things to it later)
-                cols_mlt = ['Date_UTC', 'MLT']
-                temp_data_mlt = data[cols_mlt].loc[data['IAGA'] == stations[0]]
-                temp_times_mlt = pd.to_datetime(temp_data_mlt['Date_UTC'].unique())
-                mlt = xr.DataArray(data = temp_data_mlt['MLT'],
-                                   coords = [temp_times_mlt],
-                                   dims = ['time'])
-                # loop through the stations and append each to master DataArray
-                for i in stations[1:]:
-                        temp_data_mlt = data[cols_mlt].loc[data['IAGA'] == i]
-                        temp_times_mlt = pd.to_datetime(temp_data_mlt['Date_UTC'].unique())
-                        temp_mlt = xr.DataArray(data = temp_data_mlt['MLT'],
-                                          coords = [temp_times_mlt],
-                                          dims = ['time'])
-                        mlt = xr.concat([mlt, temp_mlt], dim = 'station')
-                        mlt = mlt.transpose('time', 'station')
-                # build MLT Dataset, for merging later
-                ds_mlt = xr.Dataset(data_vars = {'mlts': (['time', 'station'], mlt)},
-                                    coords = {'time': times,
-                                              'reading': readings,
-                                              'station': stations})
-
-
-
-        #-----------------------------------------------------------------------
-        #---------- build the main DataArray of the measurements ---------------
-        #-----------------------------------------------------------------------
-
+    # if MLT (Magnetic Local Time) is included, make a Dataset
+    if MLT is True:
         # initialize DataArray (so we can append things to it later)
-        cols = np.append('Date_UTC', readings)
-        temp_data = data[cols].loc[data['IAGA'] == stations[0]]
-        temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
-        da = xr.DataArray(data = temp_data[readings],
-                          coords = [temp_times, readings],
-                          dims = ['time', 'reading'])
-
+        cols_mlt = ['Date_UTC', 'MLT']
+        temp_data_mlt = data[cols_mlt].loc[data['IAGA'] == stations[0]]
+        temp_times_mlt = pd.to_datetime(temp_data_mlt['Date_UTC'].unique())
+        mlt = xr.DataArray(data = temp_data_mlt['MLT'],
+                           coords = [temp_times_mlt],
+                           dims = ['time'])
         # loop through the stations and append each to master DataArray
         for i in stations[1:]:
-                temp_data = data[cols].loc[data['IAGA'] == i]
-                temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
-                temp_da = xr.DataArray(data = temp_data[readings],
-                                       coords = [temp_times, readings],
-                                       dims = ['time', 'reading'])
-                da = xr.concat([da, temp_da], dim = 'station')
-                da = da.transpose('time', 'reading', 'station')
-
-        # build Dataset from readings
-        ds = xr.Dataset(data_vars = {'measurements': (['time', 'reading', 'station'], da)},
-                        coords = {'time': times,
-                                  'reading': readings,
-                                  'station': stations})
-
+            temp_data_mlt = data[cols_mlt].loc[data['IAGA'] == i]
+            temp_times_mlt = pd.to_datetime(temp_data_mlt['Date_UTC'].unique())
+            temp_mlt = xr.DataArray(data = temp_data_mlt['MLT'],
+                                    coords = [temp_times_mlt],
+                                    dims = ['time'])
+            mlt = xr.concat([mlt, temp_mlt], dim = 'station')
+            mlt = mlt.transpose('time', 'station')
+        # build MLT Dataset, for merging later
+        ds_mlt = xr.Dataset(data_vars = {'mlts': (['time', 'station'], mlt)},
+                            coords = {'time': times,
+                                      'reading': readings,
+                                      'station': stations})
 
 
-        #-----------------------------------------------------------------------
-        #---------- build the final DataArray from optional arguments ----------
-        #-----------------------------------------------------------------------
 
-        # include MLT
-        if MLT is True:
-                ds = xr.merge([ds, ds_mlt])
+    #-----------------------------------------------------------------------
+    #---------- build the main DataArray of the measurements ---------------
+    #-----------------------------------------------------------------------
 
-        # include MLAT
-        if MLAT is True:
-                ds = xr.merge([ds, ds_mlat])
+    # initialize DataArray (so we can append things to it later)
+    cols = np.append('Date_UTC', readings)
+    temp_data = data[cols].loc[data['IAGA'] == stations[0]]
+    temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
+    da = xr.DataArray(data = temp_data[readings],
+                      coords = [temp_times, readings],
+                      dims = ['time', 'reading'])
+
+    # loop through the stations and append each to master DataArray
+    for i in stations[1:]:
+        temp_data = data[cols].loc[data['IAGA'] == i]
+        temp_times = pd.to_datetime(temp_data['Date_UTC'].unique())
+        temp_da = xr.DataArray(data = temp_data[readings],
+                               coords = [temp_times, readings],
+                               dims = ['time', 'reading'])
+        da = xr.concat([da, temp_da], dim = 'station')
+        da = da.transpose('time', 'reading', 'station')
+
+    # build Dataset from readings
+    ds = xr.Dataset(data_vars = {'measurements': (['time', 'reading', 'station'], da)},
+                    coords = {'time': times,
+                              'reading': readings,
+                              'station': stations})
 
 
-        return ds
+
+    #-----------------------------------------------------------------------
+    #---------- build the final DataArray from optional arguments ----------
+    #-----------------------------------------------------------------------
+
+    # include MLT
+    if MLT is True:
+        ds = xr.merge([ds, ds_mlt])
+
+    # include MLAT
+    if MLAT is True:
+        ds = xr.merge([ds, ds_mlat])
+
+
+    return ds
 ################################################################################
 
 
@@ -131,9 +130,8 @@ def mag_csv_to_Dataset(csv_file, readings=None, MLT=None, MLAT=None):
 ### Function to plot the readings like on the SuperMAG website
 #       input: ds- dataset output from mag_data_to_Dataset function
 #       output: series of plots, one per station, of the readings
-
 def plot_mag_data(ds):
-        ds.measurements.plot.line(x='time', hue='reading', col='station', col_wrap=1)
+    ds.measurements.plot.line(x='time', hue='reading', col='station', col_wrap=1)
 ################################################################################
 
 
@@ -141,45 +139,90 @@ def plot_mag_data(ds):
 
 ################################################################################
 ################################################################################
-### Function to calculate the first canonical correlation coefficients
+### Function to calculate the first canonical correlation coefficients between stations
 #       input: ds- dataset output from mag_data_to_Dataset function
 #              readings- vector of characters representing measurements, default is ['N', 'E', 'Z']
-#       output: DataArray of cca coefficients, dims are 'first_st' and 'second_st'
-
+#       output: Dataset of cca coefficients
+#                   data- cca_coeffs
+#                   coordinates- 'first_st', 'second_st'
 def inter_st_cca(ds, readings=None):
-        # check if readings are provided
-        if readings is None:
-                readings = ['N', 'E', 'Z']
+    # check if readings are provided
+    if readings is None:
+        readings = ['N', 'E', 'Z']
 
-        # universally necessary things
-        stations = ds.station
-        num_st = len(stations)
-        num_read = len(readings)
+    # universally necessary things
+    stations = ds.station
+    num_st = len(stations)
+    num_read = len(readings)
 
-        # setup (triangular) array for the correlation coefficients
-        cca_coeffs = np.zeros(shape = (num_st, num_st), dtype = float)
+    # setup (triangular) array for the correlation coefficients
+    cca_coeffs = np.zeros(shape = (num_st, num_st), dtype = float)
 
-        # shrinking nested for loops to get all the pairs of stations
-        for i in range(0, num_st-1):
-            first_st = ds.measurements.loc[dict(station = stations[i])]
-            for j in range(i+1, num_st):
-                second_st = ds.measurements.loc[dict(station = stations[j])]
-                # if False in np.isfinite(first_st) or False in np.isfinite(second_st):
-                comb_st = xr.concat([first_st, second_st], dim='reading')
-                # test stations for NaNs in the data (will mess up cca)
-                comb_st = comb_st.dropna(dim='time', how='any')
-                first_st = comb_st[:, 0:num_read]
-                second_st = comb_st[:, num_read:2*num_read]
-                # run cca
-                temp_cca = rcca.CCA(kernelcca = False, reg = 0., numCC = 1)
-                cca_coeffs[i,j] = temp_cca.train([first_st, second_st]).cancorrs[0]
+    # shrinking nested for loops to get all the pairs of stations
+    for i in range(0, num_st-1):
+        first_st = ds.measurements.loc[dict(station = stations[0])]
+        for j in range(i+1, num_st):
+            second_st = ds.measurements.loc[dict(station = stations[1])]
+            # remove NaNs from data (will mess up cca)
+            comb_st = xr.concat([first_st, second_st], dim = 'reading')
+            comb_st = comb_st.dropna(dim = 'time', how = 'any')
+            first_st = comb_st[:, 0:num_read]
+            second_st = comb_st[:, num_read:2*num_read]
+            # run cca
+            temp_cca = rcca.CCA(kernelcca = False, reg = 0., numCC = 1)
+            cca_coeffs[i,j] = temp_cca.train([first_st, second_st]).cancorrs[0]
 
-        # build DataArray from the cca_coeffs array
-        da = xr.DataArray(data = cca_coeffs,
-                          coords = [stations, stations],
-                          dims = ['first_st', 'second_st'])
+    # build DataArray from the cca_coeffs array
+    da = xr.DataArray(data = cca_coeffs,
+                      coords = [stations, stations],
+                      dims = ['first_st', 'second_st'])
 
-        # convert the DataArray into a Dataset
-        ds = da.to_dataset(name='cca_coeffs')
+    # convert the DataArray into a Dataset
+    ds = da.to_dataset(name = 'cca_coeffs')
 
-        return ds
+    return ds
+
+
+### Function to calculate the first canonical correlation coefficients between readings in one station
+#       input: ds- dataset output from mag_data_to_Dataset function
+#              station- 3 letter code for the station as a string, ex: 'BLC'
+#              readings- vector of characters representing measurements, default is ['N', 'E', 'Z']
+#       output: Dataset of cca coefficients
+#                   data- cca_coeffs
+#                   coordinates- 'first_read', 'second_read'
+def intra_st_cca(ds, station, readings=None):
+    # check if readings are provided
+    if readings is None:
+        readings = ['N', 'E', 'Z']
+
+    # universally necessary things
+    num_read = len(readings)
+
+    # get readings for the station
+    read = ds.measurements.loc[dict(station = station)]
+    # remove Nans from data (will mess up cca)
+    read = read.dropna(dim = 'time', how = 'any')
+
+    # setup (triangular) array for the correlation coefficients
+    cca_coeffs = np.zeros(shape = (num_read, num_read), dtype = float)
+
+    # shrinking nested for loops to get all the pairs of stations
+    for i in range(0, num_read-1):
+        first_read = read.loc[dict(reading = readings[i])].values
+        first_read = np.reshape(first_read, newshape=[len(first_read),1])
+        for j in range(i+1, num_read):
+            second_read = read.loc[dict(reading = readings[j])].values
+            second_read = np.reshape(second_read, newshape=[len(second_read),1])
+            # run cca
+            temp_cca = rcca.CCA(kernelcca = False, reg = 0., numCC = 1)
+            cca_coeffs[i,j] = temp_cca.train([first_read, second_read]).cancorrs[0]
+
+    # build DataArray from the cca_coeffs array
+    da = xr.DataArray(data = cca_coeffs,
+                      coords = [readings, readings],
+                      dims = ['first_read', 'second_read'])
+
+    # convert the DataArray into a Dataset
+    ds = da.to_dataset(name = 'cca_coeffs')
+
+    return (ds)
