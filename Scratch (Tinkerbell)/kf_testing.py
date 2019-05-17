f6ae24d075174823df88d33d4d7c539ae59b01e5
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2019-05-17 23:14:30 +0100

    cca works for the coefficients

--- Scratch (Tinkerbell)/kf_testing.py
+++ Scratch (Tinkerbell)/kf_testing.py
@@ -17,6 +17,10 @@ import spaceweather.rcca as rcca
 
 ccac_test = sac.cca_coeffs(ds)
 
+
+
+#### This method works for the cca coefficients ####
+
 ds = sad.csv_to_Dataset(csv_file = "Data/20190403-00-22-supermag.csv")
 components=['N', 'E', 'Z']
 
@@ -53,12 +57,12 @@ for i in range(0, num_st-1): # first station, initialize the DataArray
     # coeffs
     coeffs_arr = np.array([ccac.cancorrs[0].flatten()])
     coeffs_fs = xr.DataArray(data = coeffs_arr)
-    coeffs_fs = coeffs_fs.assign_coords(first_st = st_1.station.values[0])
-    coeffs_fs = coeffs_fs.assign_coords(second_st = st_2.station.values[0])
+    coeffs_fs = coeffs_fs.assign_coords(first_st = i)
+    coeffs_fs = coeffs_fs.assign_coords(second_st = i+1)
     coeffs_fs = coeffs_fs.squeeze(dim = ['dim_0', 'dim_1'], drop = True)
 
-    # loop through the other stations ------------------------------------------
-    # check if there are at least three stations (I think)
+    # loop through all but the last of the other stations ----------------------
+    # check if there are at least three stations
     if num_st >= 3:
         for j in range(i+2, num_st): # second station
             st_2 = ds.measurements.loc[dict(station = stations[j])]
@@ -77,18 +81,38 @@ for i in range(0, num_st-1): # first station, initialize the DataArray
             # coeffs
             cfs_arr = np.array([ccac.cancorrs[0].flatten()])
             cfs = xr.DataArray(data = cfs_arr)
-            cfs = cfs.assign_coords(first_st = st_1.station.values[0])
-            cfs = cfs.assign_coords(second_st = st_2.station.values[0])
+            cfs = cfs.assign_coords(first_st = i)
+            cfs = cfs.assign_coords(second_st = j)
             cfs = cfs.squeeze(dim = ['dim_0', 'dim_1'], drop = True)
 
             coeffs_fs = xr.concat([coeffs_fs, cfs], dim = 'second_st')
 
-    if i==0:
-        coeffs = coeffs_fs
-    else:
-        coeffs = xr.concat([coeffs, coeffs_fs], dim = first_st)
+        if i==0:
+            coeffs = coeffs_fs
+        elif i < num_st-2:
+            coeffs = xr.concat([coeffs, coeffs_fs], dim = 'first_st')
+        else: # i = num_st-1; ie the last loop
+            dummy = xr.DataArray(data = 0)
+            dummy = dummy.assign_coords(first_st = num_st-2)
+            dummy = dummy.assign_coords(second_st = num_st-2)
+            cdum = xr.concat([coeffs_fs, dummy], dim = 'second_st')
+            coeffs = xr.concat([coeffs, cdum], dim = 'first_st')
 
+# adjust the coordinates
+blank = np.zeros(shape=(num_st, num_st))
+blank_da = xr.DataArray(data = blank,
+                        coords = [range(num_st), range(num_st)],
+                        dims = ['first_st', 'second_st'])
+coeffs = coeffs.combine_first(blank_da)
 
+# mirror results across the main diagonal
+for i in range(0, num_st):
+    for j in range(i+1, num_st):
+        coeffs.values[j,i] = coeffs.values[i,j]
+
+# label the coordinates
+coeffs = coeffs.assign_coords(first_st = stations)
+coeffs = coeffs.assign_coords(second_st = stations)
 #-------------------------------------------------------------------------------
 
 
