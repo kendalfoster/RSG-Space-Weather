93fe63891e7ac4e87563ee08effe3d66c730c62c
George Watkins <gpdwatkins@gmail.com>
2019-05-21 11:51:12 +0100

    Commented out dependence on threshold.py from heatmaps.py

--- spaceweather/visualisation/heatmaps.py
+++ spaceweather/visualisation/heatmaps.py
@@ -6,7 +6,7 @@ import xarray as xr # if gives error, just rerun
 import spaceweather.rcca as rcca
 import spaceweather.analysis.cca as sac
 import spaceweather.analysis.data_funcs as sad
-import spaceweather.analysis.threshold as sat
+# import spaceweather.analysis.threshold as sat
 
 
 def plot_adj_mat(adj_mat, stations, rns):
