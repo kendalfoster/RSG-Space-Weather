1cfb5d9e5607acd51879944c4bb3b0aba362b575
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2019-05-22 17:38:26 +0100

    Reworked correlogram function (#39)
    * reworked correlogram function to return xarray.Dataset

--- Scratch (Tinkerbell)/gw_testing.py
+++ Scratch (Tinkerbell)/gw_testing.py
@@ -11,7 +11,9 @@ import spaceweather.analysis.cca as sac
 import spaceweather.analysis.data_funcs as sad
 import spaceweather.analysis.threshold as sat
 import spaceweather.rcca as rcca
+
 import spaceweather.visualisation.heatmaps as svh
+
 
 def inter_phase_dir_corr(ds,station1,station2,wind_start1,wind_start2,readings=None):
      #check if readings are provided
@@ -91,7 +93,9 @@ def phase_finder(ds, station1, station2, start):
 
 ds1 = sad.csv_to_Dataset(csv_file = "Data/20190403-00-22-supermag.csv",MLT = True, MLAT = True)
 ds2 = sad.csv_to_Dataset(csv_file = "Data/20010305-16-38-supermag.csv",MLT = True, MLAT = True)
+
 my_ds = sad.csv_to_Dataset(csv_file = "Data/20190521-14-08-supermag.csv",MLT = True, MLAT = True)
+
 
 components=['N', 'E', 'Z']
 # ds = data_funcs.mag_detrend(ds1)
@@ -111,6 +115,7 @@ def angles(ts1, ts2):
 scratch_threshold = sat.threshold(ds1)
 
 sat.adj_mat(ds1,plot=True,thr_xrds=scratch_threshold)
+
 
 
 
