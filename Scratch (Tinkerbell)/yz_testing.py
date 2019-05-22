7de8cc5c2a93b95870e0df33651347bdfc1cc082
yziw <yuanyi.zhu@warwick.ac.uk>
2019-05-22 18:30:44 +0100

    pair correlation fn + heatmap in yztesting

--- Scratch (Tinkerbell)/yz_testing.py
+++ Scratch (Tinkerbell)/yz_testing.py
@@ -35,13 +35,14 @@ import numpy as np
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import datetime
+import xarray as xr
 
 import pandas as pd
 
-N330 = sad.mag_csv_to_Dataset(csv_file = "Data/20190403-00-22-supermag.csv",
+data = sad.mag_csv_to_Dataset(csv_file = "Data/20190403-00-22-supermag.csv",
                             MLT = True, MLAT = True)
 
-t = station_readings.time[1].data
+t = N330.time[1].data
 t
 
 
@@ -52,7 +53,18 @@ def vector_normalise(v):
 
 n = len(N330.station)
 
-sum_N = sum(N330.measurements.loc[dict(component = "N", time = t)])
+sum_N = 0
+
+for s in data.station:
+    if
+
+
+np.nansum(N330.measurements.loc[dict(component = "N", time = data.time[800])])
+
+
+
+
+
 sum_E = sum(N330.measurements.loc[dict(component = "E", time = t)])
 sum_Z = sum(N330.measurements.loc[dict(component = "Z", time = t)])
 
@@ -71,34 +83,36 @@ phi = (normed_sum/(n*v_0)).data.item()
 
 phi
 
-def order_param(data, t, normed = False):
+def order_param(data, normed = False):
     #there might be some difference if i normed each reading before doing the calculations, as some stations are more
     #sensitive than others
     #need to look into dealing with missing readings - currently looks like
     #if readings missing from one station then everything is fucked. should be quite simple.
 
     n = len(data.station)
+    params = np.zeros(len(data.time))
+    i = 0
 
-    sum_N = sum(data.measurements.loc[dict(component = "N", time = t)])
-    sum_E = sum(data.measurements.loc[dict(component = "E", time = t)])
-    sum_Z = sum(data.measurements.loc[dict(component = "Z", time = t)])
-    normed_sum = np.sqrt(sum_N**2 + sum_E**2 + sum_Z**2)
-    # np.sqrt(sum(sum(N330.measurements.loc[dict(station = s, time = t)] for s in N330.station)**2))
+    for t in data.time:
+        sum_N = np.nansum(data.measurements.loc[dict(component = "N", time = t)])
+        sum_E = np.nansum(data.measurements.loc[dict(component = "E", time = t)])
+        sum_Z = np.nansum(data.measurements.loc[dict(component = "Z", time = t)])
+        normed_sum = np.sqrt(sum_N**2 + sum_E**2 + sum_Z**2)
+        # np.sqrt(sum(sum(N330.measurements.loc[dict(station = s, time = t)] for s in N330.station)**2))
 
-    v_0 = sum(np.sqrt(sum(N330.measurements.loc[dict(station = s, component = c, time = t)]**2 for c in N330.component)) for s in N330.station)/n
-    #average absolute velocity
+        v_0 = np.sum(np.sqrt(np.nansum(data.measurements.loc[dict(station = s, time = t)]**2)) for s in data.station)/n
+        #average absolute velocity
 
-    phi = (normed_sum/(n*v_0)).data.item()
-    #calculates actual order parameter
+        phi = (normed_sum/(n*v_0))#.data.item()
+        #calculates actual order parameter
+        params[i] = phi
 
-    return phi
+        i += 1
 
-order_param(N330, t)
+    return params
 
-data = N330
-times = N330.time
+plt.plot(order_param(data))
 
-def
 params = np.zeros(len(times))
 i = 0
 for t in times:
@@ -107,11 +121,134 @@ for t in times:
 
 
 i
+times.data
 
 
+def order_param_plot(data):
+    times = data.time
+    params = np.zeros(len(times))
+    i = 0
+
+    for t in times:
+        params[i] = order_param(data, t)
+        i += 1
+
+    plt.figure(figsize = (20, 8))
+    plt.plot(params)
+    plt.title("order parameter new")
+    plt.xlabel("time")
+    plt.ylabel("phi")
+
+order_param_plot(data)
+
+range(len(data.time))
+
+a = data.measurements.loc[dict(station = "BLC", time = t)]
+
+b = data.measurements.loc[dict(station = data.station[2], time = t)]
+
+
+a-b
+
+test = a-b
+np.sqrt(np.nansum(test**2))
+
+
+
+
+
+
+dr = 0.5
+r_range = np.linspace(0, 2, 21)
+t = data.time[5]
+N = len(data.station) #number of points
+pcf = np.zeros((len(r_range), len(data.time[0:100]))) #pair correlation function
+
+for time_index in range(len(data.time[0:100])):
+    t = data.time[time_index]
+    for s in data.station:
+        data.measurements.loc[dict(station = s, time = t)] = vector_normalise(data.measurements.loc[dict(station = s, time = t)].data)
+
+    for r_index in range(len(r_range)):
+        r = r_range[r_index]
+        count = 0
+
+        for s1 in data.station:
+            for s2 in data.station:
+                diff = data.measurements.loc[dict(station = s1, time = t)] - data.measurements.loc[dict(station = s2, time = t)]
+                dist = np.sqrt(np.nansum(diff**2))
+                if (max(0, r) < dist and dist < r+dr):
+                    count += 1
+
+        pcf[r_index, time_index] = (r*count)/(3*dr*N**2)
+
+
+pcf
+plt.figure(figsize = (20, 8))
+plt.pcolormesh(pcf)
+plt.xlabel("time", fontsize = 20)
+plt.ylabel("r", fontsize = 20)
+plt.colorbar()
+
+
+plt.figure(figsize = (20, 8))
+plt.plot(pcf)
+# plt.xticks(r_range)
+
+
+
+
+vector_normalise(data.measurements.loc[dict(station = "BLC", time = t)].data)
+
+
+
+
+
+
+data.measurements
+
+data.measurements.loc[dict(time = data.time[range(100)])]
+
+
+
+
+
+
+
+
+
+
+
+
+
+plt.figure(figsize = (24.5, 8))
 plt.plot(params)
+plt.title("order parameter new")
+plt.xlabel("time")
+# plt.xticks(data.time.data)
+plt.ylabel("phi")
 
-svl.plot_mag_data(data)
+plt.figure(figsize = (27, 8))
+plt.pcolormesh(pcf)
+plt.xlabel("time", fontsize = 20)
+plt.ylabel("r", fontsize = 20)
+plt.colorbar()
+
+data.measurements.loc[dict(time = data.time[range(100)])].plot.line(x='time', hue='station', col='component', figsize = (20, 8), col_wrap=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 avg_N = sum(data.measurements.loc[dict(component = "N", time = t)])/n
