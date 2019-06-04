9f63beedb3b7f2bf37521b069aecc54f04b37155
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2019-06-04 22:30:51 +0100

    added running.py

--- Report/running.py
+++ Report/running.py
@@ -1,3 +1,6 @@
+'''
+This file is meant for letting things run in the background.
+'''
 ## Packages
 import spaceweather.analysis.cca as sac
 import spaceweather.analysis.data_funcs as sad
@@ -12,16 +15,6 @@ import spaceweather.supermag as sm
 import xarray as xr
 import numpy as np
 
-
-quiet_day_ds = sad.csv_to_Dataset('Report/CSV Files/quiet-day-1998-02-02.csv', MLAT=True)
-svl.plot_mag_data(quiet_day_ds)
-# quiet_day_am = sat.adj_mat(quiet_day_ds)
-# quiet_day_am.to_netcdf(path = 'Report/Saved Datasets/quiet-day-adj-mat.nc')
-quiet_day_am = xr.open_dataset('Report/Saved Datasets/quiet-day-adj-mat.nc')
-
-
 event_ds = sad.csv_to_Dataset('Report/CSV Files/event-1997-11-05.csv', MLAT=True)
-svl.plot_mag_data(event_ds)
-# event_am = sat.adj_mat(event_ds)
-# event_am.to_netcdf(path = 'Report/Saved Datasets/event-1997-11-05-adj-mat.nc')
-event_am = xr.open_dataset('Report/Saved Datasets/event-1997-11-05-adj-mat.nc')
+event_am = sat.adj_mat(event_ds)
+event_am.to_netcdf(path = 'Report/Saved Datasets/event-1997-11-05-adj-mat.nc')
